8d097dd648d229a38954eef9b7749744a4422f23
Md Raziur Rahaman Ronju <raziur.rahaman.ronju@gmail.com>
2022-09-07 00:43:07 +0600

    initial commit

---
+++ app/src/main/java/com/eritlab/cryptotracker/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.eritlab.cryptotracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eritlab.cryptotracker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
